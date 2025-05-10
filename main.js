906ec6432494da6ae0207843a3d153c8b1557efc
Eder Silva <irmaoeder1@gmail.com>
2025-05-10 19:57:00 -0300

    Electron 09.04

--- main.js
+++ main.js
@@ -1,51 +1,194 @@
-const { app, BrowserWindow, nativeTheme } = require("electron/main")
-const path = require("node:path")
+const {
+  app,
+  BrowserWindow,
+  nativeTheme,
+  Menu,
+  shell,
+} = require("electron/main");
+const path = require("node:path");
 
 // janela principal
-let win
+let win;
 function createWindow() {
-  nativeTheme.themeSource = "dark"
+  nativeTheme.themeSource = "dark";
   win = new BrowserWindow({
     width: 1010,
     height: 720,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js")
-    }
-  })
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-  win.loadFile("./src/views/index.html")
+  // menu personalizado
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+  win.loadFile("./src/views/index.html");
 }
 
 // janela sobre
 function aboutWindow() {
-  nativeTheme.themeSource = "dark"
-  const win = new BrowserWindow({
-    width: 320,
-    height: 160,
-    autoHideMenuBar: true,
-    resizable: false,
-    minimizable: false,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js")
-    }
-  })
+  nativeTheme.themeSource = "dark";
+  const main = BrowserWindow.getFocusedWindow()
+  let about
+  if(main) {
+    about = new BrowserWindow({
+      width: 320,
+      height: 160,
+      autoHideMenuBar: true,
+      resizable: false,
+      minimizable: false,
+      parent: main,
+      modal: true
+      
+    })
+  }
 
-  win.loadFile("./src/views/sobre.html")
+  about.loadFile("./src/views/sobre.html");
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
   //aboutWindow()
 
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
     }
-  })
-})
+  });
+});
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
-    app.quit()
+    app.quit();
   }
-})
+});
+
+// menu
+const template = [
+  {
+    label: "Arquivo",
+    submenu: [
+      {
+        label: "Novo",
+        accelerator: "CmdOrCtrl+N",
+      },
+      {
+        label: "Abrir",
+        accelerator: "CmdOrCtrl+O",
+      },
+      {
+        label: "Salvar",
+        accelerator: "CmdOrCtrl+S",
+      },
+      {
+        label: "Salvar como",
+        accelerator: "CmdOrCtrl+Shift+S",
+      },
+      {
+        type: "separator",
+      },
+      {
+        label: "Sair",
+        click: () => app.quit(),
+        accelerator: "Alt+F4",
+      },
+    ],
+  },
+  {
+    label: "Editar",
+    submenu: [
+      {
+        label: "Desfazer",
+        role: "undo",
+      },
+      {
+        label: "Refazer",
+        role: "redo",
+      },
+      {
+        type: "separator",
+      },
+      {
+        label: "Recortar",
+        role: "cut",
+      },
+      {
+        label: "Copiar",
+        role: "copy",
+      },
+      {
+        label: "Colar",
+        role: "paste",
+      },
+    ],
+  },
+  {
+    label: "Zoom",
+    submenu: [
+      {
+        label: "Aplicar zoom",
+        role: "zoomIn",
+      },
+      {
+        label: "Reduzir",
+        role: "zoomOut",
+      },
+      {
+        label: "Restaurar o zoom padrão",
+        role: "resetZoom",
+      },
+    ],
+  },
+
+  {
+    label: "Cor",
+    submenu: [
+      {
+        label: "Amarelo",
+        click: () => win.webContents.send('set-color', "var(--amarelo")
+      },
+      {
+        label: "Azul",
+        click: () => win.webContents.send('set-color', "var(--azul")
+      },
+      {
+        label: "Laranja",
+        click: () => win.webContents.send('set-color', "var(--laranja")
+      },
+      {
+        label: "Pink",
+        click: () => win.webContents.send('set-color', "var(--pink")
+      },
+      {
+        label: "Roxo",
+        click: () => win.webContents.send('set-color', "var(--roxo")
+      },
+      {
+        label: "Verde",
+        click: () => win.webContents.send('set-color', "var(--verde")
+      },
+      {
+        type: "separator",
+      },
+      {
+        label: "Restaurar a cor padrão",
+        click: () => win.webContents.send('set-color', "var(--cinzaClaro")
+      },
+    ],
+  },
+  {
+    label: "Ajuda",
+    submenu: [
+      {
+        label: "Repositório",
+        click: () =>
+          shell.openExternal(
+            "https://github.com/somadekadane/mini-dev-editor.git"
+          ),
+      },
+      {
+        label: "Sobre",
+        click: () => aboutWindow()
+      },
+    ],
+  },
+];
